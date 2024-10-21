b177e53309b20e40ebb121cde9d877305020af68
jakekrol <jdkrol351@gmail.com>
2024-10-20 19:33:39 -0600

    rm: del copy py and shell scripts

--- shell/migrate_tumour_samples.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-mapfile -t files < <(cat /data/jake/gene-fusion/data/meta/tumour_samples.txt)
-(
-    cd /data/jake/gene-fusion/data/cancer_data/prostate/prostate_sort
-    for file in ${files[@]}; do
-        cp $file ../prostate_tumour_sort/$file
-    done
-)
