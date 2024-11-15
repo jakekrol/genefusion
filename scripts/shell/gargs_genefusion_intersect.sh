6dd6f5f5e3aa791d7ea4543f5e90c64a39a23cd1
jakekrol <jdkrol351@gmail.com>
2024-11-14 19:01:57 -0700

    sh: gargs interesect for pcawg

--- scripts/shell/gargs_genefusion_intersect.sh
+++ scripts/shell/gargs_genefusion_intersect.sh
@@ -2,10 +2,16 @@
 # parallel intersect giggle hits with gene file
 set -u
 
-procs=${1:-10}
+procs=${1:-64}
 script="/data/jake/genefusion/scripts/shell/giggle2fusions.sh"
-input="/data/jake/genefusion/scripts/shell/giggle2fusions.input"
 genefile="/data/jake/genefusion/data/gene_file.txt"
-outdir="/data/jake/genefusion/data/2024_11_01-fusions-1kg"
 
-gargs --log gargs_giggle2fusions_1kg.log -p $procs -o "$script -a $genefile -b {0} -o {1}" < $input
+# 1kg
+#input="/data/jake/genefusion/scripts/shell/giggle2fusions.input"
+#outdir="/data/jake/genefusion/data/2024_11_01-fusions-1kg"
+#gargs --log gargs_giggle2fusions_1kg.log -p $procs -o "$script -a $genefile -b {0} -o {1}" < $input
+
+# pcawg
+input="/data/jake/genefusion/scripts/shell/giggle2fusions-pcawg.input"
+# rm -1 lines
+gargs --log gargs_giggle2fusions_pcawg.log -p $procs -o "$script -a $genefile -b {0} -o {1}" < $input
