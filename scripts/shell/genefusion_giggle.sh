32ff81beefd852f3b7fe79ad5aed221cf632645e
jakekrol <jdkrol351@gmail.com>
2025-04-09 20:33:40 -0600

    latest giggle search script

--- scripts/shell/genefusion_giggle.sh
+++ scripts/shell/genefusion_giggle.sh
@@ -10,9 +10,6 @@ while getopts "i:f:g:c:s:l:r:o:b" opt; do
     i)
       index=$OPTARG
       ;;
-    f)
-      genefile=$OPTARG
-      ;;
     g)
       gene=$OPTARG
       ;;
@@ -39,29 +36,10 @@ while getopts "i:f:g:c:s:l:r:o:b" opt; do
       ;;
   esac
 done
-date
-if [ -z "${outfile}" ]; then
-  outfile="$chrm.$strand.$gene.$left.$right.giggle"
-fi
-# if [ -z "${outintersect}" ]; then
-#   outintersect="$chrm.$strand.$gene.$left.$right.intersect"
-# fi
 
-giggle search -v -i $index -r $chrm:$left-$right > $outfile # | cut -f 5-7 > $outfile
-# rm excord hits with all -1 interval
-# adjust to get bedtools intersect to work
-#sed -i '/^-1/d' $outfile
-# intersect with gene file
-#bedtools intersect -a $genefile -b $outfile > $outintersect
-date
+echo "searching  $gene ($chrm:$left-$right) in index $index"
+(
+  cd $index/..
+  giggle search -v -i $index -r $chrm:$left-$right > $outfile # | cut -f 5-7 > $outfile
+)
 
-#date
-#region="21:39751949-40033704"
-#index="/data/jake/genefusion/data/prostate/shards/shard_0/index"
-#gene_file="/data/jake/genefusion/data/prostate/shards/shard_0/chr21.neg.test"
-#hits=/data/jake/genefusion/data/prostate/shards/shard_0/ERG.hits
-#giggle search -v -i $index -r $region | cut -f 5-7 > $hits
-#sed -i '/^-1/d' $hits
-#bedtools intersect -a $gene_file -b $hits > bedtools_intersect.out
-## cut -f4 bedtools_intersect.out | sort | uniq -c | sort -n
-#date
