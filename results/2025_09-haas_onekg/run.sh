cae354d1feb9ef5f3004726724aa19d2fc9e23d2
jakekrol <jdkrol351@gmail.com>
2025-09-16 20:01:48 -0600

    1000 genomes depth of haas fusions

--- results/2025_09-haas_onekg/run.sh
+++ results/2025_09-haas_onekg/run.sh
@@ -7,15 +7,27 @@
     --alias_map ../2025_09-haas_interval_frac/merged_alias.yaml \
     --output 'haas_left_right_mapped.tsv'
 
+# rm unresolved gene names
+grep -v "^-1" haas_left_right_mapped.tsv > haas_left_right_mapped_cln.tsv
+
 # join haas with 1kg results
-onekg="../results/2025_09-onekg_giggle2fusion/g2f/pop_normal_fusions.tsv"
+onekg="../2025_09-onekg_giggle2fusion/g2f/pop_normal_fusions.tsv"
 
 join.py \
-    -x haas_left_right_mapped.tsv \
+    -x haas_left_right_mapped_cln.tsv \
     -y $onekg \
     -t left \
     -o haas_onekg.tsv \
     -k left,right
+
+drop_duplicates.py -i haas_onekg.tsv -o haas_onekg_no_dups.tsv -c 5
+
+# histogram of 1kg supporting reads for haas fusions
+
+tail -n +2 haas_onekg_no_dups.tsv \
+    | cut -f11 \
+    | hist.py -o haas_onekg_readcount_histogram.png \
+    --ylog -x "1000 Genomes depth" -y Frequency
 
 
 
