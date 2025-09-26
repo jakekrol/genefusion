88f47ef37ab96156166e23d6af4e8b1f08bfc488
jakekrol <jdkrol351@gmail.com>
2025-09-25 19:58:28 -0600

    make nicely formatted haas 1kg table

--- results/2025_09-haas_onekg/run.sh
+++ results/2025_09-haas_onekg/run.sh
@@ -20,36 +20,73 @@ join.py \
     -o haas_onekg.tsv \
     -k left,right
 
+# duplicates will have the same 1000 genomes evidence
+# however, they may have different haas evidence in the junction (J) or spanning (S) reads
+# future analysis may want to compare S reads with onekg reads
 drop_duplicates.py -i haas_onekg.tsv -o haas_onekg_no_dups.tsv -c 5
 
+# rm previous FusionAnnotator column
+cut -f10 --complement haas_onekg_no_dups.tsv > z && mv z haas_onekg_no_dups.tsv
+
 # fill missing 1000 genomes records with 0
-script='import pandas as pd;df=pd.read_csv("haas_onekg_no_dups.tsv",sep="\t");df["pe_count_normal"].fillna(0,inplace=True);df["pe_count_normal"] = df["pe_count_normal"].astype(int);df.to_csv("haas_onekg_no_dups_filled.tsv",sep="\t",index=False)'
+script='import pandas as pd;df=pd.read_csv("haas_onekg_no_dups.tsv",sep="\t");\
+df["pe_count_normal"].fillna(0,inplace=True);\
+df["pe_count_normal"] = df["pe_count_normal"].astype(int);\
+df = df.sort_values(by="pe_count_normal",ascending=False);\
+cols = df.columns.tolist();\
+cols.insert(2, cols.pop(cols.index("pe_count_normal")));\
+df = df[cols];\
+df.to_csv("haas_onekg_no_dups_filled.tsv",sep="\t",index=False)'
 python3 -c "$script"
 
-# histogram of 1kg supporting reads for haas fusions
+# re-run FusionAnnotator after resolving name aliases
+tail -n +2 haas_onekg_no_dups_filled.tsv | \
+    cut -f1,2 | \
+    sed 's|\t|--|' > tmp.txt
+FusionAnnotator \
+    --annotate tmp.txt \
+    --no_add_header_column \
+    --genome_lib_dir $GENOME_LIB_DIR \
+    > tmp2.txt
 
+# reformat for join
+sed 's|--|\t|' tmp2.txt > tmp3.txt
+sed -i '1ileft\tright\tannotation' tmp3.txt
+
+# join new annotation
+join.py \
+    -x haas_onekg_no_dups_filled.tsv \
+    -y tmp3.txt \
+    -t left \
+    -o haas_onekg_no_dups_filled_annotated.tsv \
+    -k left,right
+
+# mv annotation column 
+awk 'BEGIN{OFS="\t"} {print $1,$2,$3,$11,$4,$5,$6,$7,$8,$9,$10}' haas_onekg_no_dups_filled_annotated.tsv > z && mv z haas_onekg_no_dups_filled_annotated.tsv
+
+# cleanup
+rm tmp.txt tmp2.txt tmp3.txt
+
+# histogram of 1kg supporting reads for haas fusions
 tail -n +2 haas_onekg_no_dups_filled.tsv \
-    | cut -f11 \
+    | cut -f3 \
     | hist.py -o haas_onekg_readcount_histogram.png \
     --ylog -x "1000 Genomes depth" -y Frequency
 
 # inspect fusions with >10000 supporting reads in 1kg
 tail -n +2 haas_onekg_no_dups_filled.tsv \
-    | awk '$11 > 10000' | \
+    | awk '$3 > 10000' | \
     # nr numeric reverse
-    sort -k11,11nr | \
-    # reorder columns to put depth third
-    awk 'BEGIN{OFS="\t"} {print $1,$2,$11,$3,$4,$5,$6,$7,$8,$9,$10}' > haas_onekg_depth_gt10000.tsv
+    sort -k3,3nr > haas_onekg_depth_gt10000.tsv
 
 # do the same with >1000 supporting reads in 1kg
 tail -n +2 haas_onekg_no_dups.tsv \
-    | awk '$11 > 1000' | \
-    sort -k11,11nr | \
-    awk 'BEGIN{OFS="\t"} {print $1,$2,$11,$3,$4,$5,$6,$7,$8,$9,$10}' > haas_onekg_depth_gt1000.tsv  
+    | awk '$3 > 1000' | \
+    sort -k3,3nr > haas_onekg_depth_gt1000.tsv
 
+# careful! we are using the no_dups file here
 # scatter plot of haas spanning reads vs 1kg depth
 # for cases where haas spanning reads is greater than 10
-
 tail -n +2 haas_onekg_no_dups_filled.tsv | 
     # awk '$7 > 10' | \
     cut -f 7,11 | \
