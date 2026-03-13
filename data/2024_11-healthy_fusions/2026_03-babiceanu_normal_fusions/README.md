8df2dcc4173ef0d919379237464b73bb74163bd3
jakekrol <jdkrol351@gmail.com>
2026-03-13 12:59:23 -0600

    recurrent normal fusions

---
+++ data/2024_11-healthy_fusions/2026_03-babiceanu_normal_fusions/README.md
@@ -0,0 +1,1 @@
+From [Recurrent chimeric fusion RNAs in non-cancer tissues and cells](https://doi.org/10.1093/nar/gkw032)
---
+++ data/2024_11-healthy_fusions/2026_03-babiceanu_normal_fusions/run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+wget -O recurrent_normal.csv \
+    "https://docs.google.com/spreadsheets/d/1dzicCSrs1n17duJuIK8EGy1iSvLQ8JhW/export?format=csv&gid=130561229"
+wget -O all_normal.csv \
+    "https://docs.google.com/spreadsheets/d/1dzicCSrs1n17duJuIK8EGy1iSvLQ8JhW/export?format=csv&gid=868482891"
