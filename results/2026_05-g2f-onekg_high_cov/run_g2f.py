f180d8be9a9fae1d6b88908461e3a5417e7f56c4
jakekrol <jdkrol351@gmail.com>
2026-05-14 21:10:59 -0600

    high cov onekg g2f

--- results/2026_05-g2f-onekg_high_cov/run_g2f.py
+++ results/2026_05-g2f-onekg_high_cov/run_g2f.py
@@ -59,6 +59,15 @@ df_merge_pd = df_merge.to_pandas()
 print(f"# time to convert merged dataframe back to pandas dataframe: {time.time() - t_0:.2f} seconds")
 print(f"# shape of merged dataframe: {df_merge_pd.shape}")
 
+# free memory
+df_fusions=None
+df_bed=None
+df_fusions_pl=None
+df_bed_pl=None
+
+# steps=['giggle', 'clean', 'swap', 'intersect', 'evidence']
+# steps=['clean', 'swap', 'intersect', 'evidence']
+steps=['swap', 'intersect', 'evidence']
 df_evidence = giggle2fusion(
     df_merge_pd, df_shard, outdir=args.outdir_g2f, logdir=args.logdir, max_workers=args.max_workers, timeout=60*60*2, bgzip=True,
     sample_clean_func=lambda x: os.path.basename(x),
@@ -69,7 +78,11 @@ df_evidence = giggle2fusion(
     outfile_prefix='',
     burden=True,
     bedtools_bin='/data/jake/bedtools.static.binary',
-    verbose=True
+    verbose=True,
+    steps=steps
 )
+
+# free memory
+df_merge_pd=None
 
 agg_evidence_by_category(args.outdir_g2f, args.outdir_agg, df_shard)
