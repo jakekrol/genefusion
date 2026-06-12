20668b73b58cf73ab3d74b55df49fb7786499736
jakekrol <jdkrol351@gmail.com>
2026-06-12 14:15:53 -0600

    gff3 download grch37 transcripts

--- data/2026_06-grch37-transcripts/run.sh
+++ data/2026_06-grch37-transcripts/run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 wget 'https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_19/gencode.v19.annotation.gtf.gz'
+wget 'https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_19/gencode.v19.annotation.gff3.gz'
