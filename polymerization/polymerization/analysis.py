8c274f47d2a1b754945031dd1e80186df618298f
jakekrol <jdkrol351@gmail.com>
2026-06-21 19:29:03 -0600

    bp aware normal evidence counting

--- polymerization/polymerization/analysis.py
+++ polymerization/polymerization/analysis.py
@@ -41,3 +41,36 @@ def intersect2breakpoints(df_intersect, gene_right, group_by_sample=False):
     cols = ['left_breakpoint', 'right_breakpoint'] + cols
     df_breakpoints = df_breakpoints[cols]
     return df_breakpoints
+
+def bin_breakpoints(df_bp, x_edges, y_edges):
+	'''
+	bin breakpoints into 2D histogram
+	'''
+	hist, _, _ = np.histogram2d(df_bp['left_breakpoint'], df_bp['right_breakpoint'], bins=(x_edges, y_edges))
+	# swap axes to have ERG on x and TMPRSS2 on y
+	# np.histogram2d array puts x on rows and y on columns, so need to transpose to have ERG on x and TMPRSS2 on y
+	hist = hist.T
+	return hist
+
+# for reads use read_g2f_intersect(..., group_by_sample=False)
+# for samples use read_g2f_intersect(..., group_by_sample=True)
+# as inputs
+def count_breakpoint_aware_normal_evidence(list_df_bp_tumor,list_df_bp_normal, x_edges, y_edges, bins_per_dim=10):
+	'''
+	list_df_bp_tumor: list of dataframes of tumor breakpoints
+	list_df_bp_normal: list of dataframes of normal breakpoints
+	x_edges: edges for binning left breakpoints
+	y_edges: edges for binning right breakpoints
+	bins_per_dim: number of bins per dimension for histogram
+	return number of breakpoint aware reads and samples in normal that are also in tumor
+	'''
+	T= np.zeros((bins_per_dim, bins_per_dim), dtype=np.int32)
+	N= np.zeros((bins_per_dim, bins_per_dim), dtype=np.int32)
+	for df_bp in list_df_bp_tumor:
+		T += bin_breakpoints(df_bp, x_edges, y_edges)
+	for df_bp in list_df_bp_normal:
+		N += bin_breakpoints(df_bp, x_edges, y_edges)
+	# breakpoint aware evidence is the minimum of T and N in each bin
+	N_bp_aware = np.minimum(N, T)
+	evidence = N_bp_aware.sum()
+	return evidence
