3316b02aee93d949da654c011e6ccffa686eaf3c
jakekrol <jdkrol351@gmail.com>
2026-06-24 16:29:59 -0600

    bed of gene coords and bed of exon coords grch37

--- results/2026_06-grch37-transcripts/extract_gene_name.py
+++ results/2026_06-grch37-transcripts/extract_gene_name.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 import pandas as pd
+import argparse
 
-df = pd.read_csv("gencode.v19.annotation.gtf.nohead.exon", sep="\t", header=None)
+parser = argparse.ArgumentParser(description="Extract gene names, transcript ids, and exon ids from GTF file.")
+parser.add_argument("--input", type=str, required=True, help="Input GTF file without header.")
+parser.add_argument("--output", type=str, required=True, help="Output file name for the extracted data.")
+parser.add_argument("--extract_gene_name", action="store_true", help="Extract gene names.")
+parser.add_argument("--extract_transcript_id", action="store_true", help="Extract transcript ids.")
+parser.add_argument("--extract_exon_id", action="store_true", help="Extract exon ids.")
+args = parser.parse_args()
+
 
 def extract_gene_name(info_str):
     fields = info_str.split(";")
@@ -24,9 +32,33 @@ def extract_transcript_id(info_str):
             return field.strip().split('"')[1]
     return None
 
-df["gene_name"] = df[4].apply(extract_gene_name)
-df["exon_id"] = df[4].apply(extract_exon_id)
-df["transcript_id"] = df[4].apply(extract_transcript_id)
-df = df[[0, 1, 2, "gene_name", "exon_id", "transcript_id", 3]]
+def main():
+    df = pd.read_csv(args.input, sep="\t", header=None)
+    if args.extract_gene_name:
+        df["gene_name"] = df[4].apply(extract_gene_name)
+    if args.extract_exon_id:
+        df["exon_id"] = df[4].apply(extract_exon_id)
+    if args.extract_transcript_id:
+        df["transcript_id"] = df[4].apply(extract_transcript_id)
+    # df = df[[0, 1, 2, "gene_name", "exon_id", "transcript_id", 3]]
+    if args.extract_gene_name and args.extract_exon_id and args.extract_transcript_id:
+        df = df[[0, 1, 2, "gene_name", "exon_id", "transcript_id", 3]]
+    elif args.extract_gene_name and args.extract_exon_id:
+        df = df[[0, 1, 2, "gene_name", "exon_id", 3]]
+    elif args.extract_gene_name and args.extract_transcript_id:
+        df = df[[0, 1, 2, "gene_name", "transcript_id", 3]]
+    elif args.extract_exon_id and args.extract_transcript_id:
+        df = df[[0, 1, 2, "exon_id", "transcript_id", 3]]
+    elif args.extract_gene_name:
+        df = df[[0, 1, 2, "gene_name", 3]]
+    elif args.extract_exon_id:
+        df = df[[0, 1, 2, "exon_id", 3]]
+    elif args.extract_transcript_id:
+        df = df[[0, 1, 2, "transcript_id", 3]]
+    else:
+        raise ValueError("At least one of --extract_gene_name, --extract_exon_id, or --extract_transcript_id must be specified.")
 
-df.to_csv("gencode.v19.annotation.gtf.exons.bed", sep="\t", index=False, header=False)
+    df.to_csv(args.output, sep="\t", index=False, header=False)
+
+if __name__ == "__main__":
+    main()
--- results/2026_06-grch37-transcripts/run.sh
+++ results/2026_06-grch37-transcripts/run.sh
@@ -2,16 +2,42 @@
 
 gtf=../../data/2026_06-grch37-transcripts/gencode.v19.annotation.gtf.gz
 
+echo "# decompressing gtf"
 gunzip -c $gtf > gencode.v19.annotation.gtf
 
+echo "# removing header"
 grep -v "^#" gencode.v19.annotation.gtf > gencode.v19.annotation.gtf.nohead
 
+echo "# filtering for exons"
 awk -F'\t' 'BEGIN {OFS="\t"} $3 == "exon" {print $1, $4, $5, $7, $9 ,$10}' gencode.v19.annotation.gtf.nohead > gencode.v19.annotation.gtf.nohead.exon
 
+echo "# extracting gene names, trasncript ids, and exon ids"
 # produces gencode.v19.annotation.gtf.exons.bed
-./extract_gene_name.py
+./extract_gene_name.py \
+	--input gencode.v19.annotation.gtf.nohead.exon \
+	--output gencode.v19.annotation.gtf.exons.bed \
+	--extract_gene_name \
+	--extract_transcript_id \
+	--extract_exon_id
 
+echo "# sorting exons by chrom, start, end"
 sort -t$'\t' -k1,1 -k2,2n -k3,3n gencode.v19.annotation.gtf.exons.bed -o gencode.v19.annotation.gtf.exons.bed.sorted
 
+echo "# compressing exons"
 gzip -k gencode.v19.annotation.gtf.exons.bed.sorted
 
+# do the same but for gene-level only
+
+echo "# filtering for genes"
+awk -F'\t' 'BEGIN {OFS="\t"} $3 == "gene" {print $1, $4, $5, $7, $9 ,$10}' gencode.v19.annotation.gtf.nohead > gencode.v19.annotation.gtf.nohead.gene
+
+echo "# extracting gene names"
+./extract_gene_name.py \
+	--input gencode.v19.annotation.gtf.nohead.gene \
+	--output gencode.v19.annotation.gtf.nohead.gene.bed \
+	--extract_gene_name \
+	--extract_transcript_id
+
+sort -t$'\t' -k1,1 -k2,2n -k3,3n gencode.v19.annotation.gtf.nohead.gene.bed -o gencode.v19.annotation.gtf.nohead.gene.bed.sorted
+
+gzip -k gencode.v19.annotation.gtf.nohead.gene.bed.sorted
