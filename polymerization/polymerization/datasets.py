23d274b42875d7fb807c3ad7955f6f05cf7da949
jakekrol <jdkrol351@gmail.com>
2026-09-05 20:36:23 -0600

    pkg: add thousg rna star fusion calls to data

--- polymerization/polymerization/datasets.py
+++ polymerization/polymerization/datasets.py
@@ -6,160 +6,164 @@ from importlib.resources import files
 def get_data_file(filename):
     return files("polymerization.data").joinpath(filename)
 
+def get_thousg_rna_star_fusion_calls():
+    path = get_data_file("thousg_rna-star_fusion_calls.tsv")
+    return pd.read_csv(path, sep="\t")
+
 def get_babiceanu_recurrent_normal_tissue_specific_fusions():
-	path = get_data_file("babiceanu_recurrent_normal_tissue_specific_fusions.tsv")
-	return pd.read_csv(path, sep="\t")
+    path = get_data_file("babiceanu_recurrent_normal_tissue_specific_fusions.tsv")
+    return pd.read_csv(path, sep="\t")
 
 def get_babiceanu_recurrent_normal_tissue_agnostic_fusions():
-	path = get_data_file("babiceanu_recurrent_normal_tissue_agnostic_fusions.tsv")
-	return pd.read_csv(path, sep="\t")
+    path = get_data_file("babiceanu_recurrent_normal_tissue_agnostic_fusions.tsv")
+    return pd.read_csv(path, sep="\t")
 
 def get_pcawg_recurrent_tumor_fusions():
-	path = get_data_file("pcawg_recurrent_tumor_fusions.tsv")
-	return pd.read_csv(path, sep="\t")
+    path = get_data_file("pcawg_recurrent_tumor_fusions.tsv")
+    return pd.read_csv(path, sep="\t")
 
 def get_pcawg_tumor_fusions():
-	path = get_data_file("pcawg_fusions.tsv")
-	return pd.read_csv(path, sep="\t")
+    path = get_data_file("pcawg_fusions.tsv")
+    return pd.read_csv(path, sep="\t")
 
 def get_cosmic_tumor_fusions():
-	path = get_data_file("cosmic_fusions.tsv")
-	return pd.read_csv(path, sep="\t")
+    path = get_data_file("cosmic_fusions.tsv")
+    return pd.read_csv(path, sep="\t")
 
 def get_pcawg_data_types():
-	x =	{
-			'blood': 
-				{
-					'modality':
-						{
-							'dna': {
-								'specimen': ['tumor', 'normal']
-							},
-							'rna': {
-								'specimen': ['tumor']
-							}
-						}
-				},
-			'bone': 
-				{
-					'modality':
-						{
-							'dna': {
-								'specimen': ['tumor', 'normal']
-							},
-							'rna': {
-								'specimen': []
-							}
-						}
-				},
-			'breast': 
-				{
-					'modality':
-						{
-							'dna': {
-								'specimen': ['tumor', 'normal']
-							},
-							'rna': {
-								'specimen': []
-							}
-						}
-				},
-			'esophagus':
-				{
-					'modality':
-						{
-							'dna': {
-								'specimen': ['tumor', 'normal']
-							},
-							'rna': {
-								'specimen': []
-							}
-						}
-				},
-			'gallbladder':
-				{
-					'modality':
-						{
-							'dna': {
-								'specimen': ['tumor', 'normal']
-							},
-							'rna': {
-								'specimen': []
-							}
-						}
-				},
-			'headneck':
-				{
-					'modality':
-						{
-							'dna': {
-								'specimen': ['tumor', 'normal']
-							},
-							'rna': {
-								'specimen': []
-							}
-						}
+    x =    {
+            'blood': 
+                {
+                    'modality':
+                        {
+                            'dna': {
+                                'specimen': ['tumor', 'normal']
+                            },
+                            'rna': {
+                                'specimen': ['tumor']
+                            }
+                        }
+                },
+            'bone': 
+                {
+                    'modality':
+                        {
+                            'dna': {
+                                'specimen': ['tumor', 'normal']
+                            },
+                            'rna': {
+                                'specimen': []
+                            }
+                        }
+                },
+            'breast': 
+                {
+                    'modality':
+                        {
+                            'dna': {
+                                'specimen': ['tumor', 'normal']
+                            },
+                            'rna': {
+                                'specimen': []
+                            }
+                        }
+                },
+            'esophagus':
+                {
+                    'modality':
+                        {
+                            'dna': {
+                                'specimen': ['tumor', 'normal']
+                            },
+                            'rna': {
+                                'specimen': []
+                            }
+                        }
+                },
+            'gallbladder':
+                {
+                    'modality':
+                        {
+                            'dna': {
+                                'specimen': ['tumor', 'normal']
+                            },
+                            'rna': {
+                                'specimen': []
+                            }
+                        }
+                },
+            'headneck':
+                {
+                    'modality':
+                        {
+                            'dna': {
+                                'specimen': ['tumor', 'normal']
+                            },
+                            'rna': {
+                                'specimen': []
+                            }
+                        }
 
-				},
-			'kidney':
-				{
-					'modality':
-						{
-							'dna': {
-								'specimen': ['tumor', 'normal']
-							},
-							'rna': {
-								'specimen': ['tumor', 'normal']
-							}
-						}
-				},
-			'liver':
-				{
-					'modality':
-						{
-							'dna': {
-								'specimen': ['tumor', 'normal']
-							},
-							'rna': {
-								'specimen': ['tumor', 'normal']
-							}
-						}
-				},
-			'ovary':
-				{
-					'modality':
-						{
-							'dna': {
-								'specimen': ['tumor', 'normal']
-							},
-							'rna': {
-								'specimen': ['tumor']
-							}
-						}
-				},
-			'pancreas':
-				{
-					'modality':
-						{
-							'dna': {
-								'specimen': []
-							},
-							'rna': {
-								'specimen': ['tumor']
-							}
-						}
-				},
-			'prostate':
-				{
-					'modality':
-						{
-							'dna': {
-								'specimen': ['tumor', 'normal']
-							},
-							'rna': {
-								'specimen': []
-							}
-						}
-				},
-	}
-	return x
+                },
+            'kidney':
+                {
+                    'modality':
+                        {
+                            'dna': {
+                                'specimen': ['tumor', 'normal']
+                            },
+                            'rna': {
+                                'specimen': ['tumor', 'normal']
+                            }
+                        }
+                },
+            'liver':
+                {
+                    'modality':
+                        {
+                            'dna': {
+                                'specimen': ['tumor', 'normal']
+                            },
+                            'rna': {
+                                'specimen': ['tumor', 'normal']
+                            }
+                        }
+                },
+            'ovary':
+                {
+                    'modality':
+                        {
+                            'dna': {
+                                'specimen': ['tumor', 'normal']
+                            },
+                            'rna': {
+                                'specimen': ['tumor']
+                            }
+                        }
+                },
+            'pancreas':
+                {
+                    'modality':
+                        {
+                            'dna': {
+                                'specimen': []
+                            },
+                            'rna': {
+                                'specimen': ['tumor']
+                            }
+                        }
+                },
+            'prostate':
+                {
+                    'modality':
+                        {
+                            'dna': {
+                                'specimen': ['tumor', 'normal']
+                            },
+                            'rna': {
+                                'specimen': []
+                            }
+                        }
+                },
+    }
+    return x
